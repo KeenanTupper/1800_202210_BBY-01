81fe1171c91a14f4183a9454a36b5e92a1450773
Kelvin Zhou <szhou54@my.bcit.ca>
2022-03-22 14:18:21 -0700

    Tried adding firebase functionality to index and login.html (getting and sending) but only getting errors right now

--- scripts/firebase/index.js
+++ scripts/firebase/index.js
@@ -1,3 +1,21 @@
 function goToMeet() {
     window.location.replace("./meetup.html");
 }
+
+function insertName(){
+    // to check if the user is logged in:
+     firebase.auth().onAuthStateChanged(user =>{
+         if (user){
+             console.log(user.uid); // let me to know who is the user that logged in to get the UID
+            currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
+            currentUser.get().then(userDoc=>{
+                //get the user name
+                var user_Name= userDoc.data().name;
+                console.log(user_Name);
+                document.getElementById("#user-name").innerText=user_Name; 
+            })    
+        }
+    
+     })
+}
+insertName();
