fdf86ba525c7de460e22c5d31f756f10f3381318
Kelvin Zhou <szhou54@my.bcit.ca>
2022-03-30 12:37:36 -0700

    added a way to add images, need to create a way to keep images and adding images, as well as trying to make a new collection so users can be displayed

--- scripts/firebase/firebase.js
+++ scripts/firebase/firebase.js
@@ -4,8 +4,10 @@ const firebaseConfig = {
   projectId: "bby1-agile",
   storageBucket: "bby1-agile.appspot.com",
   messagingSenderId: "562116327825",
-  appId: "1:562116327825:web:5f7bfd01805a3a76a3c230"
+  appId: "1:562116327825:web:5f7bfd01805a3a76a3c230",
+  storageBucket: 'gs://bby1-agile.appspot.com'
 };
 
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+var storage = firebase.storage();
--- scripts/firebase/meetup.js
+++ scripts/firebase/meetup.js
@@ -22,12 +22,22 @@ function registerUser() {
     firebase.auth().onAuthStateChanged(user =>{
         if (user){
             console.log(user.uid); // let me to know who is the user that logged in to get the UID
-            userName = db.collection("users").doc(user.uid).get();
-            console.log(userName);
-           addCurrentUser = db.collection("meetups").doc(rndcode).set({
-                user: user.uid,
-                
-           }, {merge: true}); 
+            userDoc = db.collection("users").doc(user.uid);
+            addCurrentUser = db.collection("meetups").doc(rndcode)
+            .set({
+                user: user.uid
+           }, {merge: true});
+
+           userDoc.get().then(a=>{
+            //get the user name
+            var user_Name= a.data().name;
+            console.log(user_Name);
+            //$("#name-goes-here").text(user_Name); //jquery
+            document.getElementById("user").innerText=user_Name;
+        })    
+       } else if (!user) {
+            alert("what are you doing here?");
+            window.location.href="./pre-index.html";
        }
    
     })
@@ -40,13 +50,81 @@ function registerOwner() {
 }
 
 
+// add image and use cloud
+function showUploadedPicture(){
+    const fileInput = document.getElementById("mypic-input");   // pointer #1
+    const image = document.getElementById("mypic-goes-here");   // pointer #2
 
+    //attach listener to input file
+    //when this file changes, do something
+    fileInput.addEventListener('change', function(e){
 
-function userAddImage() {
-    if (localStorage.getItem("code") == db.collection) {
+        //the change event returns a file "e.target.files[0]"
+        var blob = URL.createObjectURL(e.target.files[0]);
 
-    }
+        //change the DOM img element source to point to this file
+        image.src = blob;    //assign the "src" property of the "img" tag
+    })
 }
+showUploadedPicture();
+
+fileInput.addEventListener('change', function(e){       //event listener
+    const pickedfile = e.target.files[0];  // file that user picked
+image.src = URL.createObjectURL(pickedfile); //event handler
+})
+
+
+var storageRef = firebase.storage().ref("images/" + user.uid + ".jpg"); // Get reference
+// Upload picked file to cloud storage
+storageRef.put(pickedfile)
+.then(function(){
+	   storageRef.getDownloadURL()
+     .then(function (url) { // Get URL of the uploaded file
+           console.log(url); // Save the URL into users collection
+           db.collection("users").doc(user.uid).update({
+               "profile-pic": url
+           })
+      })
+})
+
+function uploadUserProfilePic() {
+    // Let's assume my storage is only enabled for authenticated users 
+    // This is set in your firebase console storage "rules" tab
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        var fileInput = document.getElementById("mypic-input");   // pointer #1
+        const image = document.getElementById("mypic-goes-here"); // pointer #2
+
+        // listen for file selection
+        fileInput.addEventListener('change', function (e) {
+            var file = e.target.files[0];
+            var blob = URL.createObjectURL(file);
+            image.src = blob;            // display this image
+
+            //store using this name
+            var storageRef = storage.ref("images/" + user.uid + ".jpg"); 
+            
+            //upload the picked file
+            storageRef.put(file) 
+                .then(function(){
+                    console.log('Uploaded to Cloud Storage.');
+                })
+
+						//get the URL of stored file
+            storageRef.getDownloadURL()
+                .then(function (url) {   // Get URL of the uploaded file
+                    console.log(url);    // Save the URL into users collection
+                    db.collection("users").doc(user.uid).update({
+                        "profilePic": url
+                    })
+                    .then(function(){
+                        console.log('Added Profile Pic URL to Firestore.');
+                    })
+                })
+        })
+    })
+}
+uploadUserProfilePic();
 
 function removeCodes() {
     localStorage.removeItem("code");
