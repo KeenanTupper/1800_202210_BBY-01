4d162862506c60d0bfb0f5feace57508a60f391b
Kelvin Zhou <szhou54@my.bcit.ca>
2022-04-04 03:29:02 -0700

    updates with new data now

--- scripts/firebase/meetup.js
+++ scripts/firebase/meetup.js
@@ -5,8 +5,8 @@
 
 // get the code and display on some elements
 let rndcode;
+let imageurl;
 function getrndCode() {
-
         rndcode = window.localStorage.getItem("code");
         console.log(rndcode);
 
@@ -14,7 +14,7 @@ function getrndCode() {
             code: rndcode
         });
         if (window.localStorage.getItem("code") == null) {
-        alert("no group code")
+            return alert("no group code")
     }
     
 }   
@@ -58,13 +58,17 @@ function registerOwner() {
 
 }
 
-uploadText => {
+function uploadText() {
     firebase.auth().onAuthStateChanged(user => {
-        let text = document.querySelector("textinput").value;
-        
-    
-        document.querySelector("textinput").value="";
-    })
+        let textbox = document.getElementById("textinput");
+        console.log(textbox.value)
+        db.collection("meetups").doc(rndcode).collection("leads").add({
+            user: user.uid,
+            leadtext: textbox.value,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+            }
+        )
+        })
 }
 
 // add image and use cloud
@@ -74,102 +78,90 @@ function uploadUserProfilePic() {
 
     firebase.auth().onAuthStateChanged(user => {
         var fileInput = document.getElementById("mypic-input");   // pointer #1
-        const image = document.getElementById("mypic-goes-here"); // pointer #2
-        userDoc = db.collection("users").doc(user.uid);
-        addCurrentUser = db.collection("meetups").doc(rndcode)
-        .set({
-            user: user.uid
-       }, {merge: true});
-
-
+        // const image = document.getElementById("mypic-goes-here"); // pointer #2
+    
         // give timestamp after upload
-        var meetupLocation = db.collection("meetups").doc(window.localStorage.getItem("code"));
-        meetupLocation.set({
-                timestamp: firebase.firestore.FieldValue.serverTimestamp()
-           }, {merge: true});
         
         
 
         // listen for file selection
         fileInput.addEventListener('change', function (e) {
             var file = e.target.files[0];
-            var blob = URL.createObjectURL(file);
-            image.src = blob;            // display this image
+            //var blob = URL.createObjectURL(file);
+            //image.src = blob;            // display this image
 
 
 
             // MAKE A FOR LOOP THAT GOES THROUGH HOW MANY IMAGES AND ADDS IMAGE WITH 
-            for (i = 1; i < 5; i++) {
-                
-            };
+            rndurl = Math.floor((Math.random() * 10)) + "" + Math.floor((Math.random() * 10))
+            +  "" + Math.floor((Math.random() * 10)) + "" + Math.floor((Math.random() * 10))
+            +  "" + Math.floor((Math.random() * 10)) + "" + Math.floor((Math.random() * 10))
+            +  "" + Math.floor((Math.random() * 10)) + "" + Math.floor((Math.random() * 10));
+
             //+ "/upload" + i
-            var storageRef = storage.ref("images/" + user.uid  + ".jpg"); 
+            var storageRef = storage.ref("images/" + rndurl  + ".jpg"); 
+            
 
             //upload the picked file
             storageRef.put(file) 
-                .then(function(){
-                    console.log('Uploaded to Cloud Storage.');
-                })
+            .then(function(){
+                console.log('Uploaded to Cloud Storage.');
+                console.log(storageRef);
+                console.log(storageRef.)
+            })
 
-						//get the URL of stored file
+            
             storageRef.getDownloadURL()
                 .then(function (url) {   // Get URL of the uploaded file
-                    console.log(url);    // Save the URL into users collection
-                    db.collection("meetups").doc(window.localStorage.getItem("code")).update({
-                        "images": url
+                                        // Save the URL into users collection
+                    db.collection("meetups").doc(rndcode).collection("leads").add({
+                        user: user.uid,
+                        leadimg: url,
+                        timestamp: firebase.firestore.FieldValue.serverTimestamp()
                     })
-                    .then(function(){
-                        console.log('Added Profile Pic URL to Firestore.');
-                    })
-                })        
+                })
+            
+                    
         })
-
-        
-        
-
+    
     })
-}
+}    
+
 uploadUserProfilePic();
 
-function displayUserProfilePic() {
-    console.log("hi");
-    firebase.auth().onAuthStateChanged(function (user) {      //get user object
-        db.collection("users").doc(user.uid)                  //use user's uid
-            .get()                                            //READ the doc
-            .then(function (doc) {
-                var picUrl = doc.data().profilePic;           //extract pic url
 
-            // use this line if "mypicdiv" is a "div"
-            //$("#mypicdiv").append("<img src='" + picUrl + "'>")
-                
-            // use this line if "mypic-goes-here" is an "img" 
-            $("#mypic-goes-here").attr("src", picUrl);
-            })
-    })
-}
-displayUserProfilePic();
 
-function pullImages(){
-    db.collection("meetups").where("code", "==", rndcode)
-    .collection("leads").where("timestamp", "<" )
+function pullData() {
+    var addstuff = document.getElementById("addContent");
+    db.collection("meetups").doc(rndcode).collection("leads")
+    .orderBy("timestamp", "desc")
+    .get()
     .then((querySnapshot) => {
+        
         querySnapshot.forEach((doc) => {
             // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
+            
+            if(doc.data().leadtext) {
+                addstuff.innerHTML+="<p>" + doc.data().leadtext + "<br>"
+            + "<h6>Time: " + doc.data().timestamp.toDate()  + "</h6></p>";
+            console.log(doc.data().leadtext);
+            } else if(doc.data().leadimg){
+                addstuff.innerHTML+="<img src=\"" + doc.data().leadimg + "\">" 
+            + "<h6>Time: " + doc.data().timestamp.toDate()  + "</h6></p>";
+            }
+            console.log(doc.data().leadimg);
+
+            
         });
-    })
-    .catch((error) => {
-        console.log("Error getting documents: ", error);
     });
-}
-
-function pullText(){
 
 }
 
-function orderByTimestamp(){
-
+function repeater() {
+    setTimeout(pullData() , 5000);
 }
+
+repeater();
 
 function removeCodes() {
     localStorage.removeItem("code");
