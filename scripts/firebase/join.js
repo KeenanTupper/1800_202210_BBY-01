19c740887028d2bdbc8dbb843fca37e593fb073a
Kelvin Zhou <szhou54@my.bcit.ca>
2022-04-05 12:41:42 -0700

    fixed issue join

--- scripts/firebase/join.js
+++ scripts/firebase/join.js
@@ -1,10 +1,9 @@
 
 function getCodeNumber() {
-    let code = document.querySelector("input").value;
+    let code = document.getElementById("getnum").value;
     console.log(code);
-    window.localStorage.setItem("typedcode", "true");
     window.localStorage.setItem("code", code);
     window.location.href="./meetup.html";
 }
 
-let code = document.querySelector("input").value;
+
